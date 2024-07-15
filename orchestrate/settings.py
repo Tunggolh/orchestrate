b632bc0a94bfbe77d856a71904b38c98881be5cc
Marlo Tunggolh <mtunggolh@ubx.ph>
2024-07-15 21:52:32 +0800

    Add custom user model

--- orchestrate/settings.py
+++ orchestrate/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'organizations',
     'projects',
     'tasks',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
