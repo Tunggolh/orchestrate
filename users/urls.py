81d3cdaee4b46eb280ae703c6b6872f70fdaaa9b
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-07-28 00:21:23 +0800

    Feature: Add user serializer

---
+++ users/urls.py
@@ -0,0 +1,21 @@
+"""
+URLs for users app
+"""
+
+from django.urls import path
+
+from .views import *
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = 'users'
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('me/', ManageUserView.as_view(), name='me'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
