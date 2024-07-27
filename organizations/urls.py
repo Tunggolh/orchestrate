2abd1371b19b74b6bac84ed02acf8bf8766db551
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-07-28 02:17:16 +0800

    Feature: Add retrieve and update organization

--- organizations/urls.py
+++ organizations/urls.py
@@ -9,6 +9,7 @@ app_name = 'organizations'
 
 urlpatterns = [
     path('', OrganizationListView.as_view(), name='list'),
+    path('<int:pk>/', OrganizationRetrieveUpdateView.as_view(), name='detail'),
     path('', OrganizationCreateView.as_view(), name='create'),
     path('add_member/', AddMemberView.as_view(), name='add_member'),
     path('remove_member/', RemoveMemberView.as_view(), name='remove_member'),
--- organizations/views.py
+++ organizations/views.py
@@ -2,8 +2,12 @@
 This file contains the views for the organizations app.
 """
 
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
 from organizations.serializers import OrganizationSerializer
 from organizations.models import Organization, Membership
 
@@ -36,3 +40,43 @@ class OrganizationCreateView(generics.CreateAPIView):
             user=self.request.user,
             role=Membership.OWNER
         )
+
+
+class OrganizationRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update an organization.
+    """
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrganizationSerializer
+
+    def get_object(self):
+        pk = self.kwargs['pk']
+        return get_object_or_404(Organization, id=pk)
+
+    def retrieve(self, request, *args, **kwargs):
+        organization = self.get_object()
+
+        is_member = Membership.objects.filter(
+            organization=organization, user=request.user).exists()
+
+        if not is_member:
+            return Response(
+                {'error': 'You are not a member of this organization.'},
+                status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(organization)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        organization = self.get_object()
+        organization_owner = Membership.objects.get(
+            organization=organization, role=Membership.OWNER)
+
+        if organization_owner.user != request.user:
+            return Response(
+                {'error': 'You are not the owner of this organization.'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        return super().put(request, *args, **kwargs)
