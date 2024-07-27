b632a3cfffac65f6ad2384e5784619afbdad316c
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-07-28 01:01:16 +0800

    Feature: Add organization table

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Organization(BaseModel):
+    name = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255, unique=True, db_index=True)
