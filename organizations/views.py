897f729d99cf7f54b49a79b4dfe86844cdbd7f5d
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-07-28 02:04:22 +0800

    Feature: Add create and list organizations

--- organizations/views.py
+++ organizations/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+"""
+This file contains the views for the organizations app.
+"""
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from organizations.serializers import OrganizationSerializer
+from organizations.models import Organization, Membership
+
+
+class OrganizationListView(generics.ListAPIView):
+    """
+    List organizations of the authenticated user.
+    """
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrganizationSerializer
+
+    def get_queryset(self):
+        return Organization.objects.filter(memberships__user=self.request.user)
+
+
+class OrganizationCreateView(generics.CreateAPIView):
+    """
+    Create a new organization.
+    """
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrganizationSerializer
+
+    def perform_create(self, serializer):
+        organization = serializer.save()
+
+        Membership.objects.create(
+            organization=organization,
+            user=self.request.user,
+            role=Membership.OWNER
+        )
