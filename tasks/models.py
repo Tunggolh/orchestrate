d5bfbe9e303f58138ce62f86ec9d5b11a96a3029
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-07-30 00:48:15 +0800

    Feature: Add task and columns model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from core.models import BaseModel
+
+from projects.models import Projects
+
+
+class Columns(models.Model):
+    project = models.ForeignKey(
+        Projects, on_delete=models.CASCADE, related_name='columns')
+    name = models.CharField(max_length=50)
+    position = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('project', 'name')
+        ordering = ['position']
+
+    def __str__(self):
+        return self.name
+
+
+class Task(BaseModel):
+    title = models.CharField(max_length=255, db_index=True)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+
+    columns = models.ForeignKey(
+        Columns, on_delete=models.CASCADE, related_name='tasks')
+
+    project = models.ForeignKey(
+        Projects, on_delete=models.CASCADE, related_name='tasks')
+
+    assignee = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
